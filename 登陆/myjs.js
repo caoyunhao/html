8147f85e13110f79fe18a9b8ba39a1c75b1ccc41
Yunhao Cao <Yunhao Cao>
2016-07-21 16:22:10 +0800

    修复搜索栏、、、、、、、、

--- 登陆/myjs.js
+++ 登陆/myjs.js
@@ -1,25 +1,3 @@
-//var item_num = 1;
 $(document).ready(function(e) {
 	$('.ui.dropdown.button').dropdown();
 });
-
-//
-//$(document).ready(function(e) {
-//	alert('11');
-//	$('.ui.dropdown.button').dropdown();
-//});
-
-
-//function addItem(text){
-//	var newitem = document.createElement('div');
-//	newitem.setAttribute('class','item');
-//	newitem_id = 'newitem_' + item_num;
-//	newitem.setAttribute('id', newitem_id);
-//	document.getElementById('search_list').appendChild(newitem);
-//	
-//	var newlabel = document.createElement('div');
-//	newlabel.setAttribute('class','ui black empty circular label');
-//	document.getElementById(newitem_id).appendChild(newlabel);
-//	
-//	++item_num;
-//}
